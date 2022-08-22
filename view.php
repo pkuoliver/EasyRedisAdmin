6cd7d9aa39be81bdb02c56157d4e2ed53cba36bd
Meng Wang <pkuoliver@hotmail.com>
2022-08-22 12:06:07 +0800

    Extract js code to file.

--- view.php
+++ view.php
@@ -204,31 +204,9 @@ if ($type != 'string') { ?>
 // String
 if ($type == 'string') { ?>
 
+<script src="js/json.js"></script>
 <script>
-function syntaxHighlight(json) {
-	if (typeof json != 'string') {
-		json = JSON.stringify(json, undefined, 4);
-	}
-	json = json.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>');
-	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
-		var cls = 'number';
-		if (/^"/.test(match)) {
-			if (/:$/.test(match)) {
-				cls = 'key';
-			} else {
-				cls = 'string';
-			}
-		} else if (/true|false/.test(match)) {
-			cls = 'boolean';
-		} else if (/null/.test(match)) {
-			cls = 'null';
-		}
-		return '<span class="' + cls + '">' + match + '</span>';
-	});
-}
-
-var jsonString = <?php echo $value; ?>;
-
+	var jsonString = <?php echo $value; ?>;
 </script>
 
 <div>
